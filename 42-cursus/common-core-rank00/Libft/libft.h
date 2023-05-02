480abaac596a934b0b5af8b61ded2a77b35812ff
Roger Torrent Ahn <rtorrent@cbr2s4.42barcelona.com>
2023-05-02 20:35:43 +0200

    First day cursus!

---
+++ 42-cursus/common-core-rank00/Libft/libft.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
+/*   Updated: 2023/05/02 17:00:05 by rtorrent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/* ************************************************************************** */
+
+# include <sys/types.h>
+
+// external declarations from the libc (AKA 'authorized functions')
+void	free(void *ptr);
+void	*malloc(size_t size);
+ssize_t	write(int fildes, const void *buf, size_t nbyte);
+
+/* ************************************************************************** */
+
+// functions also found in ctype.h
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+// functions also found in stdlib.h
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+
+// functions also found in string.h
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcat(char *dst, const char *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+
+// functions also found in strings.h
+void	ft_bzero(void *s, size_t n);
+
+// additional functions
+char	*ft_itoa(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(const char *s, char c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
+char	*ft_strtrim(const char *s1, const char *set);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
