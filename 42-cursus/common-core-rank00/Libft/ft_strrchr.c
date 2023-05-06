f542733b16fc2f7c6f6b340ab288edc5468cd74d
RogerTorrent <rta@alumnos.upm.es>
2023-05-06 02:59:09 +0200

    Added ft_strchr & ft_strrchr couple

---
+++ 42-cursus/common-core-rank00/Libft/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 02:35:08 by rtorrent          #+#    #+#             */
+/*   Updated: 2023/05/06 02:57:41 by rtorrent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char *sc;
+	const char *sc_last;
+
+	sc_last = sc = ft_strchr(s, c);
+	while (sc && *sc != '\0')
+	{
+		sc_last = sc;
+		sc = ft_strchr(++sc, c);
+	}
+	return ((char *)sc_last);
+}
