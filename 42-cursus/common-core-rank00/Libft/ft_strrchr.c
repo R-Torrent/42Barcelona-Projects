f1c9cc7e5c8a07c0a111d2db6acce78aa9559c58
RogerTorrent <rta@alumnos.upm.es>
2023-05-06 19:08:00 +0200

    Improved ft_strrchr

--- 42-cursus/common-core-rank00/Libft/ft_strrchr.c
+++ 42-cursus/common-core-rank00/Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 02:35:08 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/06 03:03:26 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/06 19:07:22 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	const char *sc;
-	const char *sc_last;
+	const char *s_last;
 
-	sc_last = sc = ft_strchr(s, c);
-	while (sc && *sc)
+	s_last = s = ft_strchr(s, c);
+	while (s && *s)
 	{
-		sc_last = sc;
-		sc = ft_strchr(++sc, c);
+		s_last = s;
+		s = ft_strchr(++s, c);
 	}
-	return ((char *)sc_last);
+	return ((char *)s_last);
 }
