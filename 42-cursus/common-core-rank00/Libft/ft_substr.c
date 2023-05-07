3600a634f3d55462914e9ae761003a096f44d5b1
RogerTorrent <rta@alumnos.upm.es>
2023-05-07 14:20:37 +0200

    Added ft_substr and applied it to ft_strdup

--- 42-cursus/common-core-rank00/Libft/ft_substr.c
+++ 42-cursus/common-core-rank00/Libft/ft_substr.c
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 11:03:50 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/07 14:17:27 by rtorrent         ###   ########.fr       */
+/*   Created: 2023/05/07 12:43:27 by rtorrent          #+#    #+#             */
+/*   Updated: 2023/05/07 14:15:40 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	return (ft_substr(s, 0, ft_strlen(s)));
+	size_t	size;
+	char	*dst;
+
+	size = ft_strlen(s);
+	if (start > size)
+		return (NULL);
+	size -= start;
+	if (len < size)
+		size = len;
+	dst = malloc(++size);
+	if (dst)
+		ft_strlcpy(dst, s + start, size);
+	return (dst);
 }
