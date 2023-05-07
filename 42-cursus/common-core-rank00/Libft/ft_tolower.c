412baab2e44849cce72aa36ac7491533b5133079
RogerTorrent <rta@alumnos.upm.es>
2023-05-07 11:46:14 +0200

    Missing ft_isupper & ft_islower :(

--- 42-cursus/common-core-rank00/Libft/ft_tolower.c
+++ 42-cursus/common-core-rank00/Libft/ft_tolower.c
@@ -6,15 +6,20 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 19:54:50 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/02 20:33:04 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/07 11:42:10 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_isupper(int c)
+{
+	return ('A' <= c && c <= 'Z');
+}
+
 int	ft_tolower(int c)
 {
-	if ('A' <= c && c <= 'Z')
+	if (ft_isupper(c))
 		c |= 0x20;
 	return (c);
 }
--- 42-cursus/common-core-rank00/Libft/ft_toupper.c
+++ 42-cursus/common-core-rank00/Libft/ft_toupper.c
@@ -6,15 +6,20 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 19:47:54 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/02 19:54:15 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/07 11:43:18 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_islower(int c)
+{
+	return ('a' <= c && c <= 'z');
+}
+
 int	ft_toupper(int c)
 {
-	if ('a' <= c && c <= 'z')
+	if (ft_islower(c))
 		c &= ~0x20;
 	return (c);
 }
