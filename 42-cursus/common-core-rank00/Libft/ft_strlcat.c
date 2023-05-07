3acbfcd7df303075d94fd5eedfbdfc48e2fca16c
Roger Torrent Ahn <rtorrent@cbr10s2.42barcelona.com>
2023-05-07 19:21:23 +0200

    Repaired ft_strlcat

--- 42-cursus/common-core-rank00/Libft/ft_strlcat.c
+++ 42-cursus/common-core-rank00/Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 14:16:42 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/06 18:54:03 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/07 19:19:19 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,12 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	const char	*dst0;
-	size_t		len_dst;
-	size_t		len_src;
 
 	dst0 = dst;
-	while (size > 1 && *dst)
-		dst++, size--;
-	len_dst = dst - dst0;
-	len_src = ft_strlcpy(dst, src, size);
-	return (len_dst + len_src);
+	while (size && *dst)
+	{
+		dst++;
+		size--;
+	}
+	return (dst - dst0 + ft_strlcpy(dst, src, size));
 }
-/*
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-	char *a0 = "Hello";
-	char *b = " world!";
-	char a[40];
-	
-	strcpy(a, a0);
-	printf("%s : %u\n", a, ft_strlcat(a, b, 15));
-	strcpy(a, a0);
-	printf("%s : %zu\n", a, strlcat(a, b, 15));
-	strcpy(a, a0);
-	printf("%s : %u\n", a, ft_strlcat(a, b, 1));
-	strcpy(a, a0);
-	printf("%s : %zu\n", a, strlcat(a, b, 1));
-	strcpy(a, a0);
-	printf("%s : %u\n", a, ft_strlcat(a, b, 5));
-	strcpy(a, a0);
-	printf("%s : %zu\n", a, strlcat(a, b, 5));
-	strcpy(a, a0);
-	printf("%s : %u\n", a, ft_strlcat(a, b, 11));
-	strcpy(a, a0);
-	printf("%s : %zu\n", a, strlcat(a, b, 11));
-	strcpy(a, a0);
-	printf("%s : %u\n", a, ft_strlcat(a, b, 0));
-	strcpy(a, a0);
-	printf("%s : %zu\n", a, strlcat(a, b, 0));
-
-	return 0;
-}
-*/
