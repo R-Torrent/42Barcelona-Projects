8a3ff705bf42f4b555134ee89489bfa7759caf45
RogerTorrent <rta@alumnos.upm.es>
2023-05-09 01:45:24 +0200

    Added ft_split

---
+++ 42-cursus/common-core-rank00/Libft/ft_split.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 19:51:41 by rtorrent          #+#    #+#             */
+/*   Updated: 2023/05/09 01:35:58 by rtorrent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdbool.h>
+
+static unsigned int	counter(const char *s, char c, const char **index)
+{
+	unsigned int	nc;
+	bool		count;
+
+	nc = 0;
+	count = true;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			if (count)
+			{
+				nc++;
+				if (index)
+					*index++ = s;
+			}
+			count = false;
+		}
+		else
+			count = true;
+		s++;
+	}
+	if (index)
+		*index = NULL;
+	return (nc);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	const char	**p;
+	
+	p = malloc(sizeof (char *) * counter(s, c, NULL) + 1);
+	counter(s, c, p);
+	return ((char **)p);
+}
