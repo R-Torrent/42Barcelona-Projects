e48f23a38852d4685b609275bfdd23d65c9a19ff
Roger Torrent Ahn <rtorrent@cbr8s2.42barcelona.com>
2023-05-09 20:14:44 +0200

    ft_substr: Now returns empty string if start > ft_strlen(s)

--- 42-cursus/common-core-rank00/Libft/ft_substr.c
+++ 42-cursus/common-core-rank00/Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 12:43:27 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/08 01:41:38 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:10:17 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 
 	size = ft_strlen(s);
 	if (start > size)
-		return (NULL);
+		return (ft_calloc(1, 1));
 	size -= start;
 	if (len < size)
 		size = len;
