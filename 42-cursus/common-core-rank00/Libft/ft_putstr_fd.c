8555eb4f3123489d544ce1a35716979e6169433a
RogerTorrent <rta@alumnos.upm.es>
2023-05-09 08:18:16 +0200

    Added ft_put*_fd functions

---
+++ 42-cursus/common-core-rank00/Libft/ft_putstr_fd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 07:26:29 by rtorrent          #+#    #+#             */
+/*   Updated: 2023/05/09 08:13:16 by rtorrent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	g_fd;
+
+static void	f_aux(unsigned int ui, char *pc)
+{
+	ui = ui;
+	ft_putchar_fd(*pc, g_fd);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	g_fd = fd;
+	ft_striteri(s, f_aux);
+}
