5069d1fb2c1924c4263f4cbc398a75438ff573cb
RogerTorrent <rta@alumnos.upm.es>
2023-05-09 07:21:18 +0200

    NULL protection in ft_split

--- 42-cursus/common-core-rank00/Libft/ft_split.c
+++ 42-cursus/common-core-rank00/Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 19:51:41 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/09 03:06:14 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/09 07:18:49 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ char	**ft_split(const char *s, char c)
 	const char	**p;
 	
 	p = malloc(sizeof (char *) * counter(s, c, NULL) + 1);
-	counter(s, c, p);
+	if (p)
+		counter(s, c, p);
 	return ((char **)p);
 }
