6c99f94dd0c7dd3a4cded67e4e04018a6af5ff4d
Roger Torrent Ahn <rtorrent@cbr8s2.42barcelona.com>
2023-05-09 19:02:45 +0200

    ft_putnbr_fd: Corrected error on function name

--- 42-cursus/common-core-rank00/Libft/ft_putnbr_fd.c
+++ 42-cursus/common-core-rank00/Libft/ft_putnbr_fd.c
@@ -6,25 +6,25 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 08:03:08 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/09 17:41:39 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:01:11 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	place_digit(int n, int fd)
+static void	ft_putdgts_fd(int n, int fd)
 {
 	int	div;
 
 	div = n / 10;
 	if (div)
-		place_digit(div, fd);
-	ft_putchar('0' + n % 10, fd);
+		ft_putdgts_fd(div, fd);
+	ft_putchar_fd('0' + n % 10, fd);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 		ft_putchar_fd('-', fd);
-	place_digit(n, fd);
+	ft_putdgts_fd(n, fd);
 }
