a252af7fa8bf4cb4801e567323d0a55118b774ec
Roger Torrent Ahn <rtorrent@cbr8s2.42barcelona.com>
2023-05-09 17:44:52 +0200

    ft_putnbr_fd: Redone to skip the use of 'malloc' through 'ft_itoa'

--- 42-cursus/common-core-rank00/Libft/ft_putnbr_fd.c
+++ 42-cursus/common-core-rank00/Libft/ft_putnbr_fd.c
@@ -6,13 +6,25 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 08:03:08 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/09 08:05:53 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/09 17:41:39 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	place_digit(int n, int fd)
+{
+	int	div;
+
+	div = n / 10;
+	if (div)
+		place_digit(div, fd);
+	ft_putchar('0' + n % 10, fd);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n < 0)
+		ft_putchar_fd('-', fd);
+	place_digit(n, fd);
 }
