f56082e0f9980ba920bf94417f95e3de6a353c72
RogerTorrent <rta@alumnos.upm.es>
2023-05-10 12:34:32 +0200

    ft_striteri: Added the terminating null character into the iteration

--- 42-cursus/common-core-rank00/Libft/ft_striteri.c
+++ 42-cursus/common-core-rank00/Libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 20:11:44 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/09 03:08:25 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/10 12:22:59 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	size_t	i;
+	bool	term;
 
 	i = 0;
-	while (s[i])
+	term = false;
+	while (!term)
 	{
+		term = !s[i];
 		f(i, s + i);
 		i++;
 	}
