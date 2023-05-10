5c6b8db053ae75dff481db3b4a6283be9e13f00c
RogerTorrent <rta@alumnos.upm.es>
2023-05-10 10:28:24 +0200

    ft_atoi: Finishing touches

--- 42-cursus/common-core-rank00/Libft/ft_atoi.c
+++ 42-cursus/common-core-rank00/Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 14:03:47 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/03 17:43:50 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/10 10:26:07 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,11 @@ int	ft_atoi(const char *str)
 	n = 0;
 	while (ft_isdigit(*str))
 	{
+		n *= 10;
 		if (sign)
-			n = n * 10 - (*str - '0');
+			n -= *str - '0';
 		else
-			n = n * 10 + *str - '0';
+			n += *str - '0';
 		str++;
 	}
 	return (n);
