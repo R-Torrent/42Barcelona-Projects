8a437c832e6740d7c9eee386d69dfbad8d2409b0
RogerTorrent <rta@alumnos.upm.es>
2023-05-10 10:33:11 +0200

    ft_strlcat: Finishing touches

--- 42-cursus/common-core-rank00/Libft/ft_strlcat.c
+++ 42-cursus/common-core-rank00/Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 14:16:42 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/07 19:19:19 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/10 10:32:11 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	const char	*dst0;
+	char	*dst0;
 
 	dst0 = dst;
 	while (size && *dst)
