5bf8eab81c2c58b5106da6736697cef30b1a599c
RogerTorrent <rta@alumnos.upm.es>
2023-05-11 17:03:16 +0200

    ft_split: Now duplicates the strings into the memory block adjacent to the index

--- 42-cursus/common-core-rank00/Libft/ft_split.c
+++ 42-cursus/common-core-rank00/Libft/ft_split.c
@@ -6,46 +6,87 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 19:51:41 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/09 11:33:19 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/11 16:50:40 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	counter(const char *s, char c, const char **index)
+static void	count(const char *s, char c, size_t *nc, size_t *nw)
 {
-	size_t	nc;
-	bool	count;
+	bool	neww;
 
-	nc = 0;
-	count = true;
+	*nc = 0;
+	*nw = 0;
+	neww = true;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			if (count)
-			{
-				nc++;
-				if (index)
-					*index++ = s;
-			}
-			count = false;
+			(*nc)++;
+			if (neww)
+				(*nw)++;
+			neww = false;
 		}
 		else
-			count = true;
+			neww = true;
 		s++;
 	}
-	if (index)
-		*index = NULL;
-	return (nc);
+}
+
+static void	find_entries(const char *s, char c, char **index)
+{
+	bool	neww;
+
+	neww = true;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			if (neww)
+				*index++ = (char *)s;
+			neww = false;
+		}
+		else
+			neww = true;
+		s++;
+	}
+	*index = NULL;
+}
+
+static size_t	copy_entries(char *dst, const char *src, char c, char **index) 
+{
+	const char	*dst0;
+
+	*index = dst;
+	dst0 = dst;
+	while (*src && *src != c)
+		*dst++ = *src++;
+	*dst++ = '\0';
+	return (dst - dst0);
 }
 
 char	**ft_split(const char *s, char c)
 {
-	const char	**p;
+	size_t	nc;
+	size_t	nw;
+	size_t	size_index;
+	char	**p;
+	char	**p1;
 
-	p = malloc(sizeof (char *) * counter(s, c, NULL) + 1);
+	count(s, c, &nc, &nw);
+	size_index = sizeof (char *) * (nw + 1);
+	p = malloc(size_index + nc + nw);
 	if (p)
-		counter(s, c, p);
-	return ((char **)p);
+	{
+		find_entries(s, c, p);
+		nc = size_index;
+		p1 = p;
+		while (*p1)
+		{
+			nc += copy_entries((char *)p + nc, *p1, c, p1);
+			p1++;
+		}
+	}
+	return (p);
 }
