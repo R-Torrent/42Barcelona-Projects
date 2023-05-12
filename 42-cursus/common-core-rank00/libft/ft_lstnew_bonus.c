007c89d48a9c4e27a5b72f8b723eca81ca4b3f3e
Roger Torrent Ahn <rtorrent@cbr6s1.42barcelona.com>
2023-05-12 16:38:51 +0200

    ft_lstnew: malloc return protected

--- 42-cursus/common-core-rank00/libft/ft_lstnew_bonus.c
+++ 42-cursus/common-core-rank00/libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 12:42:27 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/12 16:22:49 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/12 16:36:21 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ t_list	*ft_lstnew(void *content)
 	t_list	*p;
 
 	p = malloc(sizeof (t_list));
-	p->content = content;
-	p->next = NULL;
+	if (p)
+	{
+		p->content = content;
+		p->next = NULL;
+	}
 	return (p);
 }
