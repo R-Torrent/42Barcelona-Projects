df946afb80b25e03ac7ac04145a29ba46bd56d03
Roger Torrent Ahn <rtorrent@cbr6s1.42barcelona.com>
2023-05-12 19:13:58 +0200

    ft_lstlast: Repaired!

--- 42-cursus/common-core-rank00/libft/ft_lstlast_bonus.c
+++ 42-cursus/common-core-rank00/libft/ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:32:44 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/12 16:22:04 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/12 19:12:45 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst)
-		lst = lst->next;
+	t_list	*nxt;
+
+	nxt = lst;
+	while (nxt)
+	{
+		lst = nxt;
+		nxt = lst->next;
+	}
 	return (lst);
 }
