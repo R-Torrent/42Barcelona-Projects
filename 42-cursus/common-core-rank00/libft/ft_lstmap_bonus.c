21d95a19745f42ab334642308bd0b7f979befeb1
RogerTorrent <rta@alumnos.upm.es>
2023-05-13 01:49:38 +0200

    ft_lstmap: Redone without ft_lstiter

--- 42-cursus/common-core-rank00/libft/ft_lstmap_bonus.c
+++ 42-cursus/common-core-rank00/libft/ft_lstmap_bonus.c
@@ -6,27 +6,27 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:24:23 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/13 01:02:52 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/05/13 01:46:59 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	*(*g_f)(void *content);
-static void	(*g_del)(void *content);
-
-static void	f_aux(void *content)
-{
-	void *const	new_content = g_f(content);
-
-	g_del(content);
-	content = new_content;
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	g_f = f;
-	g_del = del;
-	ft_lstiter(lst, f_aux);
-	return (lst);
+	t_list	*new;
+	t_list	*new1;
+
+	while (lst)
+	{
+		new1 = ft_lstnew(f(lst->content));
+		if (!new1)
+		{
+			ft_lstclear(&new, del);
+			break;
+		}
+		ft_lstadd_back(&new, new1);
+		lst = lst->next;
+	}
+	return (new);
 }
