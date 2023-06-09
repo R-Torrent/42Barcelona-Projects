242274f2ca516469c3026f81efec0dedec6573e6
RogerTorrent <rta@alumnos.upm.es>
2023-06-09 12:05:43 +0200

    libft.h (ft_islower & ft_isupper)

--- 42-cursus/libft/libft.h
+++ 42-cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/12 16:16:13 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/06/09 12:04:31 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,9 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_islower(int c);
 int		ft_isprint(int c);
+int		ft_isupper(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
