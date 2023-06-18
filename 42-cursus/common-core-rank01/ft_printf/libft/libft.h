cd6b1d2c8dc9fb4f371e517cf030205ff86722ae
RogerTorrent <rta@alumnos.upm.es>
2023-06-18 14:06:50 +0200

    libft.h: alphabetical sorting of ft_memcmp & ft_memcpy

--- 42-cursus/common-core-rank01/ft_printf/libft/libft.h
+++ 42-cursus/common-core-rank01/ft_printf/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/05/12 16:16:13 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/06/18 13:57:15 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ void	*ft_calloc(size_t count, size_t size);
 
 // functions also found in string.h
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
--- 42-cursus/libft/libft.h
+++ 42-cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/06/18 13:39:56 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/06/18 13:55:47 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ void	*ft_calloc(size_t count, size_t size);
 
 // functions also found in string.h
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
