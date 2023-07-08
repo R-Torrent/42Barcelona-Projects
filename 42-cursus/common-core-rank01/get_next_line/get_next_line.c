d3b49e5a4245af352133b169db32ebcdd7603de1
RogerTorrent <rta@alumnos.upm.es>
2023-07-08 17:45:33 +0200

    get_next_line: repaired line rebuilding from list fragments

--- 42-cursus/common-core-rank01/get_next_line/get_next_line.c
+++ 42-cursus/common-core-rank01/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 11:22:56 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/07/07 19:29:36 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/07/08 17:31:01 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,29 @@
 
 static char	*construct_line(t_list **plst)
 {
-	t_list **const	plst0 = plst;
-	size_t			size;
-	char			*p0;
-	char			*p;
+	t_list	*lst;
+	size_t	size;
+	char	*p0;
+	char	*p;
 
 	if (!*plst)
 		return (NULL);
+	lst = *plst;
 	size = 0;
-	while ((*plst)->next)
+	while (lst->next)
 	{
 		size += BUFFER_SIZE;
-		(*plst) = (*plst)->next;
+		lst = lst->next;
 	}
-	p0 = malloc(size + ft_strlcpy(NULL, (*plst)->content, 0) + 1);
+	p0 = malloc(size + ft_strlcpy(NULL, lst->content, 0) + 1);
 	p = p0;
-	plst = plst0;
-	while (p0 && *plst)
+	lst = *plst;
+	while (p0 && lst)
 	{
-		p += ft_strlcpy(p, (*plst)->content, BUFFER_SIZE);
-		(*plst) = (*plst)->next;
+		p += ft_strlcpy(p, lst->content, BUFFER_SIZE + 1);
+		lst = lst->next;
 	}
-	ft_lstclear(plst0, free);
+	ft_lstclear(plst, free);
 	return (p0);
 }
 
@@ -57,7 +58,7 @@ static int	add_to_list(t_list **plst, const char *s, size_t start, size_t len)
 	return (0);
 }
 
-static ssize_t	read_first(int fd, t_list **plst, char *dst)
+static ssize_t	read_first(int fd, t_list **plst, char *dst, char **ppos_nl)
 {
 	size_t	m;
 	ssize_t	n;
@@ -74,6 +75,7 @@ static ssize_t	read_first(int fd, t_list **plst, char *dst)
 	{
 		n += m;
 		dst[n] = '\0';
+		*ppos_nl = ft_strchr(dst, '\n');
 	}
 	return (n);
 }
@@ -86,24 +88,25 @@ char	*get_next_line(int fd)
 	char			*line;
 	char			buffer[BUFFER_SIZE + 1];
 
-	n = read_first(fd, &listed_lines[fd], buffer);
-	pos_nl = NULL;
+	n = read_first(fd, &listed_lines[fd], buffer, &pos_nl);
 	while (n > 0 && !pos_nl)
 	{
+		n = add_to_list(&listed_lines[fd], buffer, 0, BUFFER_SIZE);
+		if (n == -1)
+			break;
+		n = read(fd, buffer, BUFFER_SIZE);
+		buffer[n] = '\0';
 		pos_nl = ft_strchr(buffer, '\n');
-		if (pos_nl)
-			n = add_to_list(&listed_lines[fd], buffer, 0, pos_nl - buffer + 1);
-		else
-			n = add_to_list(&listed_lines[fd], buffer, 0, BUFFER_SIZE);
-		if (n != -1 && !pos_nl)
-			n = read(fd, buffer, BUFFER_SIZE);
 	}
-	if (n == -1)
-		return (NULL);
-	line = construct_line(&listed_lines[fd]);
-	if (pos_nl && *++pos_nl)
-		add_to_list(&listed_lines[fd], buffer, pos_nl - buffer, BUFFER_SIZE);
-	return (line);
+	if (n != -1 && pos_nl)
+		n = add_to_list(&listed_lines[fd], buffer, 0, pos_nl - buffer + 1);
+	if (n != -1)
+		line = construct_line(&listed_lines[fd]);
+	if (n != -1 && pos_nl && *++pos_nl)
+		n = add_to_list(&listed_lines[fd], buffer, pos_nl - buffer, BUFFER_SIZE);
+	if (n != -1)
+		return (line);
+	return (NULL);
 }
 
 char	*ft_strchr(const char *s, int c)
