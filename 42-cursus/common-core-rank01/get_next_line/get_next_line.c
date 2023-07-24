824b32b4ebc87f2cfd09e22a5f179ee77698c18f
RogerTorrent <rta@alumnos.upm.es>
2023-07-24 02:18:10 +0200

    get_next_line: fixed 'no-characters' case

--- 42-cursus/common-core-rank01/get_next_line/get_next_line.c
+++ 42-cursus/common-core-rank01/get_next_line/get_next_line.c
@@ -6,65 +6,62 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 11:22:56 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/07/23 07:31:05 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/07/24 02:10:55 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-struct s_block
+typedef struct s_block
 {
 	char	*str;
 	size_t	len;
-};
+}	*t_pblock;
 
 static void	del_block(void *content)
 {
-	struct s_block *const	block = content;
-
-	free(block->str);
-	free(block);
+	free(((t_pblock)content)->str);
+	free(content);
 }
 
-static char	*construct_line(t_list **plst)
+static void	construct_line(t_list **plst, char **ln)
 {
 	t_list			*lst;
 	size_t			size;
-	char			*p;
 
 	lst = *plst;
 	size = 0;
 	while (lst)
 	{
-		size += ((struct s_block *)(lst->content))->len;
+		size += ((t_pblock)lst->content)->len;
 		lst = lst->next;
 	}
 	lst = *plst;
-	p = malloc(size + 1);
-	if (!lst || !p)
-		return (NULL);
-	p += size;
-	*p = '\0';
-	while (lst)
+	*ln = malloc(size + 1);
+	if (*ln)
 	{
-		size = ((struct s_block *)(lst->content))->len;
-		p = ft_memcpy(p - size, ((struct s_block *)(lst->content))->str, size);
-		lst = lst->next;
+		*ln += size;
+		**ln = '\0';
+		while (lst)
+		{
+			size = ((t_pblock)lst->content)->len;
+			*ln = ft_memcpy(*ln - size, ((t_pblock)lst->content)->str, size);
+			lst = lst->next;
+		}
 	}
 	ft_lstclear(plst, del_block);
-	return (p);
 }
 
 static ssize_t	add_block(t_list **plst, const char *str, size_t len, char **ln)
 {
-	struct s_block *const	new_block = malloc(sizeof(struct s_block));
-	char *const				new_substr = ft_substr(str, 0, len);
-	t_list *const			new_link = ft_lstnew(new_block);
+	char *const		new_substr = ft_substr(str, 0, len);
+	t_pblock const	new_blck = malloc(sizeof(struct s_block));
+	t_list *const	new_link = ft_lstnew(new_blck);
 
-	if (!new_block || !new_substr || !new_link)
+	if (!new_substr || !new_blck || !new_link)
 	{
-		free(new_block);
 		free(new_substr);
+		free(new_blck);
 		free(new_link);
 		ft_lstclear(plst, del_block);
 		if (*ln)
@@ -74,8 +71,8 @@ static ssize_t	add_block(t_list **plst, const char *str, size_t len, char **ln)
 		}
 		return (-1);
 	}
-	new_block->str = new_substr;
-	new_block->len = len;
+	new_blck->str = new_substr;
+	new_blck->len = len;
 	new_link->next = *plst;
 	*plst = new_link;
 	return (len);
@@ -87,8 +84,8 @@ static ssize_t	read_first(int fd, t_list **plst, char *dst, char **pp_nl)
 
 	if (*plst)
 	{
-		n = ((struct s_block *)((*plst)->content))->len;
-		ft_memcpy(dst, ((struct s_block *)((*plst)->content))->str, n);
+		n = ((t_pblock)(*plst)->content)->len;
+		ft_memcpy(dst, ((t_pblock)(*plst)->content)->str, n);
 		ft_lstclear(plst, del_block);
 	}
 	else
@@ -105,26 +102,26 @@ char	*get_next_line(int fd)
 {
 	ssize_t			n;
 	static t_list	*listed_lns[MAX_FILES];
-	char			buf[BUFFER_SIZE + 1];
+	char			buffer[BUFFER_SIZE + 1];
 	char			*p_nl;
 	char			*ln;
 
-	n = read_first(fd, &listed_lns[fd], buf, &p_nl);
+	n = read_first(fd, &listed_lns[fd], buffer, &p_nl);
 	ln = NULL;
 	while (n > 0 && !p_nl)
 	{
-		n = add_block(&listed_lns[fd], buf, n, &ln);
+		n = add_block(&listed_lns[fd], buffer, n, &ln);
 		if (n != -1)
-			n = read(fd, buf, BUFFER_SIZE);
+			n = read(fd, buffer, BUFFER_SIZE);
 		if (n != -1)
-			buf[n] = '\0';
+			buffer[n] = '\0';
 		if (n != -1)
-			p_nl = ft_strchr(buf, '\n');
+			p_nl = ft_strchr(buffer, '\n');
 	}
 	if (n != -1 && p_nl)
-		n = add_block(&listed_lns[fd], buf, p_nl - buf + 1, &ln);
-	if (n != -1)
-		ln = construct_line(&listed_lns[fd]);
+		n = add_block(&listed_lns[fd], buffer, p_nl - buffer + 1, &ln);
+	if (n != -1 && listed_lns[fd])
+		construct_line(&listed_lns[fd], &ln);
 	if (n != -1 && p_nl && *++p_nl)
 		n = add_block(&listed_lns[fd], p_nl, ft_strchr(p_nl, '\0') - p_nl, &ln);
 	return (ln);
