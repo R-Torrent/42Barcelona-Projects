588bf7e8453cc12037ed43a83b6506cba5103549
Roger Torrent Ahn <rtorrent@car9s5.42barcelona.com>
2023-12-14 23:05:01 +0100

    Redoing pipex without '/bin/bash' call...

--- 42-cursus/common-core-rank02/pipex/pipex.c
+++ 42-cursus/common-core-rank02/pipex/pipex.c
@@ -6,18 +6,25 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 21:41:44 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/12/13 14:13:13 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/12/14 22:09:22 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	terminate_error(int fd0, int fd1)
+{
+	perror(filename);
+	if (fd0 != -1)
+		close(fd0);
+	if (fd1 != -1)
+		close(fd1);
+	exit(EXIT_FAILURE);
+}
+
 // code for args[0] replaces 'getenv("SHELL")', of the 'stdlib' header
 void	set_args_open_files(char *args[], char **env, int argc_m1, char *argv[])
 {
-	const int	fd0 = open(argv[1], O_RDONLY);
-	const int	fd1 = open(argv[argc_m1], O_CREAT | O_WRONLY,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 	args[0] = NULL;
 	while (*env && !*args)
@@ -30,6 +37,15 @@ void	set_args_open_files(char *args[], char **env, int argc_m1, char *argv[])
 		*args = DEFAULT_SHELL;
 	args[1] = "-c";
 	args[3] = NULL;
+
+void	open_files(const char *const infile, const char *const outfile, const int append)
+	int	fd0;
+	int	fd1 = open(argv[argc_m1], O_CREAT | O_WRONLY | append,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	fd0 = open(argv[1], O_RDONLY);
+	if (fd0 == -1)
+		exit(EXIT_SUCCESS);
 	if (fd0 == -1 || fd1 == -1 || dup2(fd0, 0) == -1 || dup2(fd1, 1) == -1)
 		terminate_error(fd0, fd1, argv[0]);
 	if (close(fd0) != -1 && close(fd1) != -1)
@@ -38,51 +54,33 @@ void	set_args_open_files(char *args[], char **env, int argc_m1, char *argv[])
 	exit(EXIT_FAILURE);
 }
 
-void	terminate_error(int fd0, int fd1, char *filename)
-{
-	perror(filename);
-	if (fd0 != -1)
-		close(fd0);
-	if (fd1 != -1)
-		close(fd1);
-	exit(EXIT_FAILURE);
-}
-
-void	redirect_fds(int pipe_fd1, int pipe_fd2, int new_fd, char *filename)
-{
-	const int	status0 = dup2(pipe_fd2, new_fd);
-	const int	status1 = close(pipe_fd1);
-	const int	status2 = close(pipe_fd2);
-
-	if (status0 == -1 || status1 == -1 || status2 == -1)
-		terminate_error(0, 1, filename);
-}
-
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	*args[4];
-	int		fd[2];
-	pid_t	child_pid;
+	char *const	command_name = ft_strrchr(argv[0], '/') + 1;
+	int			fd[2];
+	pid_t		child_pid;
 
-	set_args_open_files(args, envp, --argc, argv);
+	if (argc < 4)
+		terminate_error(command_name, 2, 0, 1);
+	open_files(argv[1], argv[--argc]);
 	while (--argc)
 	{
-		args[2] = argv[argc];
 		if (argc > 2)
 		{
 			if (pipe(fd) == -1)
-				terminate_error(0, 1, argv[0]);
+				terminate_error(2, 0, 1);
 			child_pid = fork();
 			if (child_pid == -1)
-				terminate_error(fd[0], fd[1], argv[0]);
+				terminate_error(command_name, 4, fd[0], fd[1], 0, 1);
 			else if (!child_pid)
-			{
-				redirect_fds(fd[0], fd[1], 1, argv[0]);
-				continue ;
-			}
-			redirect_fds(fd[1], fd[0], 0, argv[0]);
+				if (close(fd[0]) | dup2(fd[1], 1) == -1 | close(fd[1]))
+					terminate_error(command_name, 2, 0, 1);
+				else
+					continue ;
+			if (close(fd[1]) | dup2(fd[0], 0) == -1 | close(fd[0]))
+				terminate_error(command_name, 2, 0, 1);
 		}
 		execve(args[0], args, envp);
-		terminate_error(0, 1, argv[0]);
+		terminate_error(command_name, 0, 1);
 	}
 }
