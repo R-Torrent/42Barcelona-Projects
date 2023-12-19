0469607a69eb17c7373e3d3373d590a28b9a16f4
Roger Torrent Ahn <rtorrent@car6s4.42barcelona.com>
2023-12-19 12:03:31 +0100

    Simplified ft_getenv

--- 42-cursus/common-core-rank02/pipex/ft_getenv.c
+++ 42-cursus/common-core-rank02/pipex/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 01:38:15 by rtorrent          #+#    #+#             */
-/*   Updated: 2023/12/16 03:28:17 by rtorrent         ###   ########.fr       */
+/*   Updated: 2023/12/19 12:00:39 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,15 @@
 char	*ft_getenv(const char *name)
 {
 	extern char	**environ;
-	char *const	name_eq = ft_strjoin(name, "=");
-	const int	n = ft_strlen(name_eq);
+	const int	n = ft_strlen(name);
 	int			i;
 
 	i = 0;
 	while (environ[i])
 	{
-		if (!ft_strncmp(environ[i], name_eq, n))
-		{
-			free(name_eq);
-			return (environ[i] + n);
-		}
+		if (!ft_strncmp(environ[i], name, n) && environ[i][n] == '=')
+			return (environ[i] + n + 1);
 		i++;
 	}
-	free(name_eq);
 	return (NULL);
 }
