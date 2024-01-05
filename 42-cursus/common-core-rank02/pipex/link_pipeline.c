5bc54a7349d7244b92b537037141ecbbcc4894f5
RogerTorrent <rta@alumnos.upm.es>
2024-01-05 02:22:48 +0100

    Recursion installed in command redirection

--- 42-cursus/common-core-rank02/pipex/link_pipeline.c
+++ 42-cursus/common-core-rank02/pipex/link_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 12:26:24 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/01/04 22:22:26 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/01/05 02:18:31 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,10 @@ void	seek_binary(t_data *const pdata, char **pbinary, char *word)
 	char	**paths;
 
 	if (ft_strchr(word, '/'))
+	{
+		paths = NULL;
 		*pbinary = ft_strdup(word);
+	}
 	else
 	{
 		word = ft_strjoin("/", word);
@@ -54,7 +57,7 @@ void	seek_binary(t_data *const pdata, char **pbinary, char *word)
 		}
 		free(word);
 	}
-	if (access(*pbinary, F_OK))
+	if ((!paths && access(*pbinary, F_OK)) || (paths && !*paths))
 		child_exit(pdata, COMMAND_NOT_FOUND, 0);
 	if (access(*pbinary, X_OK))
 		child_exit(pdata, COMMAND_NOT_EXECUTABLE, 0);
@@ -62,34 +65,32 @@ void	seek_binary(t_data *const pdata, char **pbinary, char *word)
 
 void	redir_command(t_data *const pdata, const t_list *redir)
 {
-	t_redir		*rdr;
-	int			flags;
-	const int	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	int			target;
-	int			fd;
+	t_redir *const	rdr = redir->content;
+	int				flags;
+	const int		mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	int				target;
+	int				fd;
 
-	while (redir)
+	flags = O_RDONLY;
+	target = STDIN_FILENO;
+	if (rdr->type == RDR_OUTPUT || rdr->type == RDR_APPOUT)
 	{
-		rdr = redir->content;
-		flags = O_RDONLY;
-		target = STDIN_FILENO;
-		if (rdr->type == RDR_OUTPUT || rdr->type == RDR_APPOUT)
-		{
-			flags = O_CREAT | O_WRONLY;
-			if (rdr->type == RDR_APPOUT)
-				flags |= O_APPEND;
-			target = STDOUT_FILENO;
-		}
-		fd = open(rdr->word, flags, mode);
-		if ((fd == -1) | (dup2(fd, target) == -1) | close(fd))
-			child_exit(pdata, EXIT_FAILURE, 0);
-		redir = redir->next;
+		flags = O_CREAT | O_WRONLY;
+		if (rdr->type == RDR_APPOUT)
+			flags |= O_APPEND;
+		target = STDOUT_FILENO;
 	}
+	fd = open(rdr->word, flags, mode);
+	if ((fd == -1) | (dup2(fd, target) == -1) | close(fd))
+		child_exit(pdata, EXIT_FAILURE, 0);
+	if (redir->next)
+		redir_command(pdata, redir->next);
 }
 
 void	exec_pln(t_data *const pdata, t_comm *const comm, char *const *envp)
 {
-	redir_command(pdata, comm->redir);
+	if (comm->redir)
+		redir_command(pdata, comm->redir);
 	seek_binary(pdata, &comm->binary, *comm->words);
 	execve(comm->binary, comm->words, envp);
 	child_exit(pdata, EXIT_FAILURE, 0);
