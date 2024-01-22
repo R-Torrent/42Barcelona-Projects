202eb34f2da4b6aa09bc4de329615c89381555a0
RogerTorrent <rta@alumnos.upm.es>
2024-01-23 00:08:20 +0100

    Incorporated 'ft_lsttoarray.c' from 'pipex' project

---
+++ 42-cursus/libft/ft_lsttoarray.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lsttoarray.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 21:16:04 by rtorrent          #+#    #+#             */
+/*   Updated: 2024/01/22 22:15:49 by rtorrent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	**ft_lsttoarray(t_list *lst)
+{
+	size_t	n;
+	void	**a;
+	t_list	*next;
+
+	n = ft_lstsize(lst);
+	a = malloc((n + 1) * sizeof(void *));
+	a[n] = NULL;
+	while (n--)
+	{
+		next = lst->next;
+		a[n] = lst->content;
+		free(lst);
+		lst = next;
+	}
+	return (a);
+}
--- 42-cursus/libft/libft.h
+++ 42-cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/01/01 18:11:07 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/01/22 22:27:50 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +115,7 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 size_t	ft_lstsize(t_list *lst);
+void    **ft_lsttoarray(t_list *lst);
 
 /* ************************************************************************** */
 
