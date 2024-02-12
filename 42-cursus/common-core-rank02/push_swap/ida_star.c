b4a78ea11c2015cef194468536455a289727e61b
RogerTorrent <rta@alumnos.upm.es>
2024-02-12 18:48:35 +0100

    Heuristic function was wrong. Elementary counterexamples found.

--- 42-cursus/common-core-rank02/push_swap/ida_star.c
+++ 42-cursus/common-core-rank02/push_swap/ida_star.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 22:33:53 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/12 18:33:59 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/12 18:46:17 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,27 +31,32 @@ unsigned int	search(t_node **ppath, unsigned int bound, t_node *temp_nodes,
 
 /* ************************************************************************** */
 /*                                                                            */
-/*   Heuristic cost: unordered, consecutive integers on stack 'a' + ordered,  */
-/*   consecutive integers in stack 'b' (a PA command will reverse ordering)   */
-/*   + number of elements in stack 'b'                                        */
+/*   Heuristic cost: max{unordered consecutive entries on stack 'a', ordered  */
+/*   consecutive entries in stack 'b'} (a PA command will reverse ordering    */
+/*   of the latter) + number of elements in stack 'b'                         */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	heuristic(t_node *node, size_t n)
 {
-	unsigned int	h;
+	unsigned int	h_a;
+	unsigned int	h_b;
 	size_t			i;
 
-	h = node->n[B];
+	h_a = node->n[B];
 	i = 0;
 	while (++i < node->n[A])
 		if (node->stacks[i] > node->stacks[i - 1])
-			h++;
+			h_a++;
+	h_b = node->n[B];
 	i = node->n[A];
 	while (++i < n)
 		if (node->stacks[i] < node->stacks[i -1])
-			h++;
-	return (h);
+			h_b++;
+	if (h_a < h_b)
+		return (h_b);
+	else
+		return (h_a);
 }
 
 int	ida_star(t_node **ppath, size_t n, t_node *temp_nodes)
