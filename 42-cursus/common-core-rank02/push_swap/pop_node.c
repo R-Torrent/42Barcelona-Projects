e2a0a553e95da9a741cc4e13c79a5faea7b194cc
RogerTorrent <rta@alumnos.upm.es>
2024-02-13 17:49:02 +0100

    Popped element discarded; code simplified

--- 42-cursus/common-core-rank02/push_swap/pop_node.c
+++ 42-cursus/common-core-rank02/push_swap/pop_node.c
@@ -6,22 +6,20 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 00:23:11 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/10 02:11:48 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/13 17:47:13 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_node	*pop_node(t_node **ppath, t_node *node, size_t size_node)
+void	pop_node(t_node **ppath)
 {
 	t_node *const	top = *ppath;
 
 	if (*ppath)
 	{
 		*ppath = (*ppath)->camefrom;
-		ft_memcpy(node, top, size_node);
 		if (!(top->moves % DEFAULT_BATCH_SZE))
 			free(top);
 	}
-	return (*ppath);
 }
