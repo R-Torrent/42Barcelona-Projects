59377cde16d48dbbc361350875141a921c125ae5
RogerTorrent <rta@alumnos.upm.es>
2024-02-15 23:59:51 +0100

    Inserted op_string declaration in the header

--- 42-cursus/common-core-rank02/push_swap/push_swap.h
+++ 42-cursus/common-core-rank02/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 22:54:19 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/15 23:11:50 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/15 23:57:13 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,7 @@ typedef struct s_info
 
 int		ida_star(t_node **ppath, t_info *pinfo);
 int		init_root(t_info *pinfo, unsigned int n, char *args[]);
+char	*op_string(enum e_ops op);
 t_node	*operate_stacks(t_node *path, enum e_ops op, t_info *pinfo);
 void	pop_node(t_node **ppath);
 void	push_node(t_node **ppath, t_info *pinfo, enum e_ops op, int *status);
