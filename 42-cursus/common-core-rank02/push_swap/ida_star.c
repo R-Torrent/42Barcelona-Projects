917540b38a300dc941d75c70f61eae33dd230b31
RogerTorrent <rta@alumnos.upm.es>
2024-02-17 00:52:05 +0100

    Added checks on successor operations to avoid duplicate branches

--- 42-cursus/common-core-rank02/push_swap/ida_star.c
+++ 42-cursus/common-core-rank02/push_swap/ida_star.c
@@ -6,17 +6,49 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 22:33:53 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/16 23:51:22 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/17 00:46:36 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+bool	in_path(t_node *path, t_node *node, t_info *pinfo)
+{
+	size_t	i;
+
+	if (node && (!path->camefrom || !in_path(path->camefrom, node, pinfo)))
+	{
+		if (path->n[A] != node->n[A])
+			return (false);
+		i = 0;
+		while (i < pinfo->n_args)
+		{
+			if (path->stacks[i] != node->stacks[i])
+				return (false);
+			i++;
+		}	
+	}
+	return (true);
+}
+
+bool	try_successor(t_node *path, enum e_ops succ, t_info *pinfo)
+{
+	if ((succ == SA && path->n[A] < 2) || (succ == SB && path->n[B] < 2)
+		|| (succ == SS && (path->n[A] < 2 || path->n[B] < 2))
+		|| (succ == PA && !path->n[B]) || (succ == PB && !path->n[A])
+		|| ((succ == RA || succ == RRA) && path->n[A] < 3)
+		|| ((succ == RB || succ == RRB) && path->n[B] < 3)
+		|| (succ == RRR && (path->n[A] < 3 || path->n[B] < 3)))
+		return (false);
+	return (!in_path(path, operate_stacks(path, succ, pinfo), pinfo));
+}
+
 /* ************************************************************************** */
 /*                                                                            */
 /*   Heuristic cost: max{unordered consecutive entries on stack 'a', ordered  */
 /*   consecutive entries in stack 'b'} (a PA command will reverse ordering    */
-/*   of the latter) + number of elements in stack 'b'                         */
+/*   of the latter) + number of elements in stack 'b' + possibly an extra     */
+/*   move if the top of stack 'b' > top of stack 'a'.                         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,25 +78,6 @@ unsigned int	heuristic(t_node *node, size_t n)
 		return (h_a);
 }
 
-bool	in_path(t_node *path, t_node *node, t_info *pinfo)
-{
-	size_t	i;
-
-	if (node && (!path->camefrom || !in_path(path->camefrom, node, pinfo)))
-	{
-		if (path->n[A] != node->n[A])
-			return (false);
-		i = 0;
-		while (i < pinfo->n_args)
-		{
-			if (path->stacks[i] != node->stacks[i])
-				return (false);
-			i++;
-		}	
-	}
-	return (true);
-}
-
 unsigned int	search(t_node **ppath, unsigned int bound, t_info *pinfo,
 	int *pstatus)
 {
@@ -80,7 +93,7 @@ unsigned int	search(t_node **ppath, unsigned int bound, t_info *pinfo,
 	successor = SA;
 	while (successor < ID)
 	{
-		if (!in_path(*ppath, operate_stacks(*ppath, successor, pinfo), pinfo))
+		if (try_successor(*ppath, successor, pinfo))
 		{
 			push_node(ppath, pinfo, successor, pstatus);
 			threshold = search(ppath, bound, pinfo, pstatus);
