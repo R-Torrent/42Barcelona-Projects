176fe8ed26f05e63347910a0ef3b318e4d04cbe5
RogerTorrent <rta@alumnos.upm.es>
2024-02-17 21:43:00 +0100

    Bonus checker created!

--- 42-cursus/common-core-rank02/push_swap/checker_bonus.c
+++ 42-cursus/common-core-rank02/push_swap/checker_bonus.c
@@ -1,16 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   checker_bonus.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 00:03:27 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/16 01:13:07 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/17 21:30:35 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
+
+bool	get_op(enum e_ops *pop, const char *instruction, int *pstatus)
+{
+	*pop = SA;
+	while (*pop < ID)
+	{
+		if (!ft_strncmp(op_string(*pop), instruction, 3))
+			return (true);
+		(*pop)++;
+	}
+	*pstatus = UNK_INS;
+	return (false);
+}
+
+bool	get_ins(char **pinstruction, int *pstatus)
+{
+	char	*c;
+
+	*pinstruction = ft_getnextline_fd(1);
+	if (*pinstruction)
+	{
+		c = ft_strchr(*pinstruction, '\n');
+		if (c)
+			*c = '\0';
+		return (true);
+	}
+	*pstatus = NOT_FND;
+	return (false);
+}
+
+void	run_ins(t_node *node, t_info *pinfo, int *pstatus)
+{
+	char		*instruction;
+	enum e_ops	op;
+
+	if (is_goal(node, pinfo, pstatus))
+		return ;
+	if (get_ins(&instruction, pstatus) && get_op(&op, instruction, pstatus))
+		operate_stacks(node, op, pinfo);
+	free(instruction);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -20,16 +61,13 @@ int	main(int argc, char *argv[])
 
 	if (!--argc)
 		exit(SUCCESS);
+	status = init_root(&info, (unsigned int)argc, argv);
 	progress = malloc(info.size_node);
-	if (!progress)
+	if (!progress && status == WORKING)
 		status = MEM_ERR;
-	else
-		status = init_root(&info, (unsigned int)argc, argv);
 	while (status == WORKING)
-	{
-		ft_memcpy(progress, info.temp_nodes0, info.size_node);
-		status = run_instruction(&info, progress);
-	}
+		run_ins(ft_memcpy(progress, info.temp_nodes0, info.size_node),
+			&info, &status);
 	if (status == SUCCESS)
 		ft_putendl_fd("OK", 1);
 	else if (status == NOT_FND)
