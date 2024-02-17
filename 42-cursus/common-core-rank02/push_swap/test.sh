ea1f2206892dd7c75bd0579030165de253d4c80b
RogerTorrent <rta@alumnos.upm.es>
2024-02-17 23:49:51 +0100

    Prepared random integer tests for the evaluations

--- 42-cursus/common-core-rank02/push_swap/test.sh
+++ 42-cursus/common-core-rank02/push_swap/test.sh
@@ -7,7 +7,16 @@
 if [ -n "$1" ]; then N=$1; else N=0; fi
 
 ARG=$(od -An -N$((4*$N)) -i /dev/random | tr -d '\n')
+echo $ARG >input.dat
 
-echo $ARG
-./push_swap $ARG > output.txt
-cat output.txt | wc -l
+./push_swap $ARG >output.dat
+RES=$?
+if [ $RES -ne 0 ]; then
+	echo "*Error* detected: Exit code $RES"
+else
+	echo "Number of instructions: $(cat output.dat | wc -l)"
+	echo "MAC checker: $(./checker_Mac $ARG <output.dat)"
+	if [ -f "checker" ]; then
+		echo "BONUS checker: $(./checker $ARG <output.dat)"
+	fi
+fi
