bc6f2b7496ed38240108da24962614a8b49e7c82
RogerTorrent <rta@alumnos.upm.es>
2024-02-20 20:08:51 +0100

    Small fixes

--- 42-cursus/common-core-rank02/push_swap/aprox.c
+++ 42-cursus/common-core-rank02/push_swap/aprox.c
@@ -6,13 +6,14 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 20:12:02 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/20 00:43:18 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/20 20:07:56 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	det_rots(size_t idxa, t_cand *pcand, size_t *rots_a, size_t *rots_b)
+void	det_rots(const size_t idxa, t_cand *pcand,
+			const size_t *rots_a, const size_t *rots_b)
 {
 	t_cand		opts[4];
 	enum e_rot	rot;
@@ -40,27 +41,32 @@ void	det_rots(size_t idxa, t_cand *pcand, size_t *rots_a, size_t *rots_b)
 			*pcand = opts[rot];
 }
 
-size_t	find_lower(size_t *n, size_t idxa)
+size_t	find_lower(const size_t *stack_b, const size_t nb, const size_t val_a)
 {
-	(void) n;
-	return (idxa); // *** WORKING HERE
+	if (nb < 2)
+		return (0);
+	// *** WORKING HERE
+	(void)stack_b;
+	(void)val_a;
+	return (0);
 }
 
-void	check_candidate(size_t *n, size_t idxa, t_cand *pcand)
+void	check_candidate(t_node *path, const size_t idxa, t_cand *pcand)
 {
 	size_t			rots_a[2];
 	size_t			rots_b[2];
-	const size_t	idxb = find_lower(n, idxa);
+	const size_t	idxb = find_lower(path->stacks + path->n[A], path->n[B],
+			path->stacks[idxa]);
 
-	rots_a[0] = n[A] - 1 - idxa;
+	rots_a[0] = path->n[A] - 1 - idxa;
 	rots_a[1] = 0;
-	if (n[A] > 1)
+	if (path->n[A] > 1)
 		rots_a[1] = idxa + 1;
 	rots_b[0] = 0;
 	rots_b[1] = 0;
-	if (n[B] > 1)
+	if (path->n[B] > 1)
 	{
-		rots_b[0] = n[B] - 1 - idxb;
+		rots_b[0] = path->n[B] - 1 - idxb;
 		rots_b[1] = idxb + 1;
 	}
 	det_rots(idxa, pcand, rots_a, rots_b);
@@ -76,7 +82,7 @@ void	send_to_b(t_node **ppath, t_info *pinfo, int *pstatus)
 	min = (t_cand){.rots_t = pinfo->n_args};
 	while (elem_a < (*ppath)->n[A])
 	{
-		check_candidate((*ppath)->n, elem_a++, &cand);
+		check_candidate(*ppath, elem_a++, &cand);
 		if (cand.rots_t < min.rots_t || (cand.rots_t == min.rots_t
 				&& (*ppath)->stacks[cand.idxa] > (*ppath)->stacks[min.idxa]))
 			min = cand;
@@ -105,10 +111,10 @@ void	aprox(t_node **ppath, t_info *pinfo, int *pstatus)
 		send_to_b(ppath, pinfo, pstatus);
 	while ((*ppath)->stacks[0] && *pstatus == WORKING)
 	{
-		if ((*ppath)->stacks[0] >= pinfo->n_args / 2)
-			push_node(ppath, op_stacks(*ppath, RB, pinfo), RB, pstatus);
+		if ((*ppath)->stacks[0] > pinfo->n_args / 2)
+			push_node(ppath, op_stacks(*ppath, RB, pinfo), RRB, pstatus);
 		else
-			push_node(ppath, op_stacks(*ppath, RRB, pinfo), RRB, pstatus);
+			push_node(ppath, op_stacks(*ppath, RRB, pinfo), RB, pstatus);
 	}
 	while ((*ppath)->n[B] && *pstatus == WORKING)
 		push_node(ppath, op_stacks(*ppath, PA, pinfo), PA, pstatus);
