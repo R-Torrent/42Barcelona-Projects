50d38e0c988722fb9f76400c7d73b6536ea13992
RogerTorrent <rta@alumnos.upm.es>
2024-02-22 21:33:26 +0100

    File opening & closing

--- 42-cursus/common-core-rank02/FdF/fdf.c
+++ 42-cursus/common-core-rank02/FdF/fdf.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 20:48:07 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/02/22 20:49:52 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/22 21:24:46 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
-	return (0);
+	int	fdfmap;
+
+	if (argc != 2)
+		exit(EXIT_FAILURE);
+	fdfmap = open(argv[1], O_RDONLY);
+	if (fdfmap == -1)
+		exit(EXIT_FAILURE);
+// PROGRAM GOES HERE
+	if (close(fdfmap) == -1)
+		exit(EXIT_FAILURE);	
+	exit(EXIT_SUCCESS);
 }
