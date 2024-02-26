e734b234f468a763ece5bff181c7e55dfd4a2df6
RogerTorrent <rta@alumnos.upm.es>
2024-02-26 20:01:49 +0100

    Copied ft_strcspn.c and ft_strspn.c from 'pipex'

--- 42-cursus/common-core-rank02/FdF/libft/libft.h
+++ 42-cursus/common-core-rank02/FdF/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:19:33 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/01/30 19:49:31 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/02/26 19:58:52 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
+size_t	ft_strcspn(const char *s, const char *reject);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -82,6 +83,7 @@ size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 char	*ft_strrchr(const char *s, int c);
+size_t	ft_strspn(const char *s, const char *accept);
 
 // functions also found in strings.h
 void	ft_bzero(void *s, size_t n);
