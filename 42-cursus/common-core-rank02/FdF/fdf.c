9851814d6eb6225e0efe13c8d17dc7a842141c0b
RogerTorrent <rta@alumnos.upm.es>
2024-03-05 04:58:44 +0100

    Coded 'fit_screen_size' function

--- 42-cursus/common-core-rank02/FdF/fdf.c
+++ 42-cursus/common-core-rank02/FdF/fdf.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 20:48:07 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/03/04 02:52:08 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/03/05 04:45:32 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,37 @@ int	key_hook(int keycode, void *param[2])
 	return (0);
 }
 
-void	fit_screen_size(t_map *map_fdf, int size_x, int size_y)
+void	draw_wires(void *mlx_ptr, void *win_ptr, t_map_fdf *map_fdf)
 {
+}
 
+void	fit_screen_size(t_map_fdf *map_fdf, int size_x, int size_y)
+{
+	t_point	*p;
+	int		limits[4];
+
+	limits[0] = INT_MAX;
+	limits[1] = INT_MIN;
+	limits[2] = INT_MAX;
+	limits[3] = INT_MIN;
+	p = map_fdf->points + map_fdf->rows * map_fdf->cols;
+	while (p-- > map_fdf->points)
+	{
+		if (p->x < limits[0])
+			limits[0] = p->x;
+		if (p->x > limits[1])
+			limits[1] = p->x;
+		if (p->y < limits[2])
+			limits[2] = p->y;
+		if (p->y > limits[3])
+			limits[3] = p->y;
+	}
+	p = map_fdf->points + map_fdf->rows * map_fdf->cols;
+	while (p-- > map_fdf->points)
+	{
+		p->x = (p->x - limits[0]) * size_x / (limits[1] - limits[0]);
+		p->y = (p->y - limits[2]) * size_y / (limits[3] - limits[2]);
+	}
 }
 
 // includes a 1000-factor rescale, to mantain significant digits in the map
@@ -32,9 +60,8 @@ void	isometric_projection(t_map_fdf *map_fdf)
 	int		yip;
 
 	p = map_fdf->points + map_fdf->rows * map_fdf->cols;
-	while (p >= map_fdf->points)
+	while (p-- > map_fdf->points)
 	{
-		p--;
 		xip = 707 * (p->x - p->z);
 		yip = 408 * (p->x + (p->y << 1) + p->z);
 		p->x = xip;
@@ -61,12 +88,9 @@ int	main(int argc, char *argv[])
 	win_ptr = mlx_new_window(mlx_ptr, PIX_X, PIX_Y, "FdF");
 	if (!win_ptr)
 		exit(EXIT_FAILURE);
-
-// GRAPHIC stuff, such as
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffffff);
-
-	mlx_key_hook(win_ptr, key_hook, (void *)(void *[2]){mlx_ptr, win_ptr});
+	draw_wires(mlx_ptr, win_ptr, map_fdf);
 	free(map_fdf);
+	mlx_key_hook(win_ptr, key_hook, (void *)(void *[2]){mlx_ptr, win_ptr});
 	mlx_loop(mlx_ptr);
 	exit(EXIT_SUCCESS);
 }
