eac52fef07c261bd3d18ed5e7dee56c8a6512949
RogerTorrent <rta@alumnos.upm.es>
2024-03-18 03:08:24 +0100

    Error management overhauled

--- 42-cursus/common-core-rank02/FdF/fdf.c
+++ 42-cursus/common-core-rank02/FdF/fdf.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <rtorrent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 20:48:07 by rtorrent          #+#    #+#             */
-/*   Updated: 2024/03/17 18:12:13 by rtorrent         ###   ########.fr       */
+/*   Updated: 2024/03/18 03:03:03 by rtorrent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,40 @@ void	exit_fdf(t_data_fdf *data, int exit_status)
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 	free(data->map);
 	exit(exit_status);
+}
+
+unsigned int	fdf_pixel(void *mlx_ptr, struct s_img *img, int *x,
+	unsigned int color_trgb)
+{
+	char		*pixel;
+	const int	bytes_per_pixel = img->bits_per_pixel / CHAR_BIT;
+
+	if (x[0] >= 0 && x[0] < PIX_X && x[1] >= 0 && x[1] < PIX_Y)
+	{
+		pixel = img->addr + (x[0] * img->size_line + x[1] * bytes_per_pixel);
+		*(unsigned int *)pixel = mlx_get_color_value(mlx_ptr, color_trgb);
+	}
+	return (color_trgb);
+}
+
+void	fdf_clear_image(void *mlx_ptr, struct s_img *img)
+{
+	int					x;
+	int					y;
+	char				*pixel;
+	const int			bytes_per_pixel = img->bits_per_pixel / CHAR_BIT;
+	const unsigned int	black = mlx_get_color_value(mlx_ptr, BLACK);
+
+	x = PIX_X;
+	while (x--)
+	{
+		y = PIX_Y;
+		while (y--)
+		{
+			pixel = img->addr + (x * img->size_line + y * bytes_per_pixel);
+			*(unsigned int *)pixel = black;
+		}
+	}
 }
 
 int	key_hook(int keycode, t_data_fdf *data)
@@ -57,46 +91,13 @@ int	key_hook(int keycode, t_data_fdf *data)
 	return (0);
 }
 
-unsigned int	fdf_pixel(void *mlx_ptr, struct s_img *img, int *x,
-	unsigned int color_trgb)
-{
-	char		*pixel;
-	const int	bytes_per_pixel = img->bits_per_pixel / CHAR_BIT;
-
-	if (x[0] >= 0 && x[0] < PIX_X && x[1] >= 0 && x[1] < PIX_Y)
-	{
-		pixel = img->addr + (x[0] * img->size_line + x[1] * bytes_per_pixel);
-		*(unsigned int *)pixel = mlx_get_color_value(mlx_ptr, color_trgb);
-	}
-	return (color_trgb);
-}
-
-void	fdf_clear_image(void *mlx_ptr, struct s_img *img)
-{
-	int					x;
-	int					y;
-	char				*pixel;
-	const int			bytes_per_pixel = img->bits_per_pixel / CHAR_BIT;
-	const unsigned int	black = mlx_get_color_value(mlx_ptr, BLACK);
-
-	x = PIX_X;
-	while (x--)
-	{
-		y = PIX_Y;
-		while (y--)
-		{
-			pixel = img->addr + (x * img->size_line + y * bytes_per_pixel);
-			*(unsigned int *)pixel = black;
-		}
-	}
-}
-
 int	main(int argc, char *argv[])
 {
 	t_data_fdf		fdf;
 	struct s_img	img;
 
 	fdf = (t_data_fdf){mlx_init(), NULL, &img, NULL};
+	img.img_ptr = NULL;
 	if (argc == 2 && fdf.mlx_ptr && !read_data(&fdf.map, argv[1]))
 	{
 		fdf.win_ptr = mlx_new_window(fdf.mlx_ptr, PIX_Y, PIX_X, argv[1]);
